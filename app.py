8e8ef28a538a1b152cdcbbf970c0a2c4d657eb67
Yufei Lin <yufei_lin_ulyssess@outlook.com>
2020-04-08 21:43:40 -0400

    Added Results

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 from land import Land
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
-#Look at the change over a period of time
+# Initialize plot
+fig, ax = plt.subplots()
+
+# Look at the change over a period of time
 generations = 10
 
-#Transition Matrix
+# Transition Matrix
 transit = np.matrix([
     [0.7, 0.4, 0.5],
     [0.2, 0.4, 0.3],
     [0.1, 0.2, 0.2]
 ])
 
-#Initialize a land
-l = Land(3, 2, 3, transit)
-print(l.land)
+# Initialize a land
+l = Land(5, 5, 3, transit)
+im = ax.imshow(l.land)
+im.figure.savefig("Original.png")
 
-for i in range (generations):
+for i in range(generations):
+    fileName = "Gen-"+str(i+1)+".png"
     l = l.withTransitionMatrix()
-    print(l.land)
+    heat = ax.imshow(l.land)
+    heat.figure.savefig(fileName)
+
